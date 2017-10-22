95863468b8782e2aafe834bfc0f687a04b93b800
Karsten Schmidt <k@postspectacular.com>
2017-10-22 15:42:51 +0100

    add Tick, OHLC interfaces, add Period enum

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,32 @@
+export interface Tick {
+    ask: number;
+    bid: number;
+    mid: number;
+    spread: number;
+    epoch: number;
+}
+
+export interface OHLC {
+    open: number;
+    high: number;
+    low: number;
+    close: number;
+    epoch: number;
+    timeISO: string;
+    volume: number;
+}
+
+export enum Period {
+    M1 = 60,
+    M5 = Period.M1 * 5,
+    M15 = Period.M1 * 15,
+    M30 = Period.M1 * 30,
+    H1 = Period.M1 * 60,
+    H4 = Period.H1 * 4,
+    DAY = Period.H1 * 24,
+    WEEK = Period.DAY * 7,
+}
+
 export interface Bounds {
     min: number;
     max: number;
